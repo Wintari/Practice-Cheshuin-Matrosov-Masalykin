e2fb32c08a110e13ebad1a6d1c12c57d99bc6f1f
MatrosovDenis <51385717+MatrosovDenis@users.noreply.github.com>
2020-07-03 17:24:29 +0300

    Add GUI files

---
+++ sources/SaveFileController.java
@@ -0,0 +1,49 @@
+package sample;
+
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.TextField;
+        import javafx.stage.DirectoryChooser;
+        import javafx.stage.FileChooser;
+        import javafx.stage.Stage;
+
+        import java.io.File;
+
+public class SaveFileController {
+
+    @FXML
+    private Button CancelButton;
+
+    @FXML
+    private Button ConfirmButton;
+
+    @FXML
+    private TextField TextField;
+
+    @FXML
+    void Confirm(){
+        Stage stage = (Stage)ConfirmButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void Cancel(){
+        Stage stage = (Stage)CancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void ChooseDirectory(){
+        final FileChooser fileChooser = new FileChooser();
+        TextField.clear();
+
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Graph", "*.graph"));
+
+        File file = fileChooser.showSaveDialog((Stage)TextField.getScene().getWindow());
+        if (file != null) {
+            TextField.setText(file.getPath());
+        }
+    }
+}
+
